bd8ca80ee685d9dfdec00e50af11cadcb0ad7a28
Josemaría García de Polavieja Ferre <jgf@altair.edu.es>
2025-01-27 13:13:26 +0100

    No tener en cuenta el componente con tabla de material

--- src/app/componentes/menu/serviciosp/listado-de-servicios/listado-de-servicios.component.ts
+++ src/app/componentes/menu/serviciosp/listado-de-servicios/listado-de-servicios.component.ts
@@ -1,28 +1,45 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MatTableModule, MatTable } from '@angular/material/table';
+import { MatTableModule, MatTable, MatTableDataSource } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
-import { ListadoDeServiciosDataSource, ListadoDeServiciosItem } from './listado-de-servicios-datasource';
+import { ListadoDeServiciosDataSource } from './listado-de-servicios-datasource';
+import { FirestoreService } from '../../../../servicios/firestore.service';
+import { Serviciosp } from '../../../../shared/modelos/serviciosp';
 
 @Component({
   selector: 'app-listado-de-servicios',
   templateUrl: './listado-de-servicios.component.html',
   styleUrls: ['./listado-de-servicios.component.css'],
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule]
+  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
 })
 export class ListadoDeServiciosComponent implements AfterViewInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  @ViewChild(MatTable) table!: MatTable<ListadoDeServiciosItem>;
-  dataSource = new ListadoDeServiciosDataSource();
+  @ViewChild(MatTable) table!: MatTable<any>;
+  //dataSource = new ListadoDeServiciosDataSource();
+  dataSource = new MatTableDataSource();
 
+  constructor(private afs: FirestoreService) {
+
+  }
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
-  displayedColumns = ['id', 'name'];
+  displayedColumns = ['id', 'nombre', 'horas', 'precioPorHora'];
 
   ngAfterViewInit(): void {
-    this.dataSource.sort = this.sort;
-    this.dataSource.paginator = this.paginator;
-    this.table.dataSource = this.dataSource;
+    this.afs.getCollection('serviciosp').subscribe((resp) => {
+        console.log(resp);
+    if (resp && resp.length > 0) {
+        this.dataSource = new MatTableDataSource(resp);
+        this.dataSource.data = resp;
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+        this.table.dataSource = this.dataSource;
+      } else {
+        console.error('Los datos recibidos están vacíos o son inválidos');
+      }
+
+      //this.table.dataSource = this.dataSource;
+    });
   }
 }
